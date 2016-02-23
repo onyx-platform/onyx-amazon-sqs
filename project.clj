2e2992c7903bbbcf212cb8bed11c70ed98ca02be
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-02-24 00:07:27 +0800

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject onyx-amazon-sqs "0.8.11.0"
+(defproject onyx-amazon-sqs "0.8.11.1-SNAPSHOT"
   :description "Onyx plugin for Amazon SQS"
   :url "https://github.com/onyx-platform/onyx-amazon-sqs"
   :license {:name "Eclipse Public License"
