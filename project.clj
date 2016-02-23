a2929a595216c64f0660512c1a93000722d9d3ee
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-02-24 00:03:58 +0800

    Add repos for auto release

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :url "https://github.com/onyx-platform/onyx-amazon-sqs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :repositories {"snapshots" {:url "https://clojars.org/repo"
+                              :username :env
+                              :password :env
+                              :sign-releases false}
+                 "releases" {:url "https://clojars.org/repo"
+                             :username :env
+                             :password :env
+                             :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.onyxplatform/onyx "0.8.12-SNAPSHOT"]
                  [com.amazonaws/aws-java-sdk "1.10.49"]]
