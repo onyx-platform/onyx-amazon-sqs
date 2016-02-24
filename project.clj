0355546efc701cd9ad5723405643f16f6c3502bd
Michael Drogalis <mjd3089@rit.edu>
2016-02-23 16:58:26 -0800

    Freshen deps.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.7.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.8.11"]
+                 [org.onyxplatform/onyx "0.8.12-20160220_014415-gced67dd"]
                  [com.amazonaws/aws-java-sdk "1.10.49"]]
   :global-vars  {*warn-on-reflection* true}
   :profiles {:dev {:dependencies [[amazonica "0.3.50"]]
