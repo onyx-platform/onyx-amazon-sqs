62b826cacd286402052e83044c19eaaf7fe62204
Lucas Bradstreet <lucasbradstreet@gmail.com>
2016-02-25 21:59:15 +0800

    Second signature for task bundles

--- src/onyx/plugin/tasks/sqs.clj
+++ src/onyx/plugin/tasks/sqs.clj
@@ -47,44 +47,48 @@
           :onyx/batch-size max-batch-size}))
 
 (s/defn ^:always-validate sqs-input
-  [task-name :- s/Keyword 
-   region :- s/Str
-   deserializer-fn :- os/NamespacedKeyword 
-   idle-backoff-ms :- s/Int 
-   task-opts :- {s/Any s/Any}]
-  {:task {:task-map (merge {:onyx/name task-name
-                            :onyx/plugin :onyx.plugin.sqs-input/input
-                            :onyx/type :input
-                            :onyx/medium :sqs
-                            :onyx/batch-size 10
-                            :onyx/batch-timeout 1000
-                            :sqs/region region
-                            :sqs/deserializer-fn deserializer-fn
-                            :sqs/attribute-names []
-                            :sqs/idle-backoff-ms idle-backoff-ms
-                            :onyx/doc "Reads segments from an SQS queue"}
-                           task-opts)
-          :lifecycles []}
-   :schema {:task-map SQSInputTaskMap
-            :lifecycles [os/Lifecycle]}})
+  ([task-name task-opts]
+   {:task {:task-map (merge {:onyx/name task-name
+                             :onyx/plugin :onyx.plugin.sqs-input/input
+                             :onyx/type :input
+                             :onyx/medium :sqs
+                             :onyx/batch-size 10
+                             :onyx/batch-timeout 1000
+                             :sqs/attribute-names []
+                             :onyx/doc "Reads segments from an SQS queue"}
+                            task-opts)
+           :lifecycles []}
+    :schema {:task-map SQSInputTaskMap
+             :lifecycles [os/Lifecycle]}})
+  ([task-name :- s/Keyword 
+    region :- s/Str
+    deserializer-fn :- os/NamespacedKeyword 
+    idle-backoff-ms :- s/Int 
+    task-opts :- {s/Any s/Any}]
+   (sqs-input task-name (merge {:sqs/region region
+                                :sqs/deserializer-fn deserializer-fn
+                                :sqs/idle-backoff-ms idle-backoff-ms}
+                               task-opts))))
 
 (s/defn ^:always-validate sqs-output
-  [task-name :- s/Keyword 
-   region :- s/Str
-   serializer-fn :- os/NamespacedKeyword 
-   task-opts :- {s/Any s/Any}]
-  {:task {:task-map (merge {:onyx/name task-name
-                            :onyx/plugin :onyx.plugin.sqs-output/output
-                            :onyx/type :output
-                            :onyx/medium :sqs
-                            :onyx/batch-size 10
-                            :sqs/region region
-                            :sqs/serializer-fn serializer-fn
-                            :onyx/doc "Writes segments to SQS queues"}
-                           task-opts)
-          :lifecycles []}
-   :schema {:task-map SQSOutputTaskMap
-            :lifecycles [os/Lifecycle]}})
+  ([task-name :- s/Keyword task-opts :- {s/Any s/Any}]
+   {:task {:task-map (merge {:onyx/name task-name
+                             :onyx/plugin :onyx.plugin.sqs-output/output
+                             :onyx/type :output
+                             :onyx/medium :sqs
+                             :onyx/batch-size 10
+                             :onyx/doc "Writes segments to SQS queues"}
+                            task-opts)
+           :lifecycles []}
+    :schema {:task-map SQSOutputTaskMap
+             :lifecycles [os/Lifecycle]}})
+  ([task-name :- s/Keyword 
+    region :- s/Str
+    serializer-fn :- os/NamespacedKeyword 
+    task-opts :- {s/Any s/Any}]
+   (sqs-output task-name (merge {:sqs/region region
+                                 :sqs/serializer-fn serializer-fn}
+                                task-opts))))
 
 ;; TODO, remove once this is in core
 (s/defn add-task :- os/Job
