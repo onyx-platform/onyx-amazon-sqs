e8fe23386e8886a8e287985fe41166068c5da67f
OnyxBot <michael.drogalis@onyxplatform.org>
2016-03-08 07:52:07 +0000

    Update dependency org.onyxplatform/onyx to version 0.9.0-alpha3.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ;^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  ; Temporarily depend on new onyx which has a breaking change for internal-ack-segment
-                 [org.onyxplatform/onyx "0.9.0-alpha1"]
+                 [org.onyxplatform/onyx "0.9.0-alpha3"]
                  [com.amazonaws/aws-java-sdk "1.10.49"]]
   :global-vars  {*warn-on-reflection* true}
   :profiles {:dev {:dependencies []
