c66f7cac7c66f603077073e992bcf255f994fc10
Gardner Vickers <gardner@vickers.me>
2016-03-31 18:56:30 -0400

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-amazon-sqs "0.9.0.2"
+(defproject org.onyxplatform/onyx-amazon-sqs "0.9.0.3-SNAPSHOT"
   :description "Onyx plugin for Amazon SQS"
   :url "https://github.com/onyx-platform/onyx-amazon-sqs"
   :license {:name "Eclipse Public License"
