ff7dc5f80c0b7679f8083d689971c8c94ee24c18
Lucas Bradstreet <lucasbradstreet@gmail.com>
2017-02-08 00:45:48 -0800

    Upgrade to newer aws-java-sdk.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.10.0-alpha4"]
-                 [com.amazonaws/aws-java-sdk "1.10.49"]]
+                 [com.amazonaws/aws-java-sdk "1.11.76"]]
   :global-vars  {*warn-on-reflection* true}
   :resource-paths ["resources/"]
   :profiles {:dev {:dependencies []
