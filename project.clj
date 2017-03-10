0e91de2bb1bb6e4c2b545e4594ed00d545d732d1
OnyxBot <michael.drogalis@onyxplatform.org>
2017-03-10 17:53:38 +0000

    Prepare for next release cycle.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-amazon-sqs "0.10.0.0-beta6"
+(defproject org.onyxplatform/onyx-amazon-sqs "0.10.0.0-SNAPSHOT"
   :description "Onyx plugin for Amazon SQS"
   :url "https://github.com/onyx-platform/onyx-amazon-sqs"
   :license {:name "Eclipse Public License"
