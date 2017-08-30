8e5e322053175c5603b01b22cbcf45ada4af8a9b
Stephen Hopper <stephenmhopper@gmail.com>
2017-08-30 16:04:45 -0500

    Updated AWS dependency to use SQS directly.

--- project.clj
+++ project.clj
@@ -14,7 +14,7 @@
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
                  [org.onyxplatform/onyx "0.10.0"]
-                 [com.amazonaws/aws-java-sdk "1.11.76"]]
+                 [com.amazonaws/aws-java-sdk-sqs "1.11.185"]]
   :global-vars  {*warn-on-reflection* true}
   :resource-paths ["resources/"]
   :profiles {:dev {:dependencies []
