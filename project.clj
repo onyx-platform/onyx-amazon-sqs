35e08dabfd202b384f59e2d0562fa193394ea058
OnyxBot <michael.drogalis@onyxplatform.org>
2017-09-13 23:07:50 +0000

    Release version 0.11.0.0-alpha5.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject org.onyxplatform/onyx-amazon-sqs "0.11.0.0-SNAPSHOT"
+(defproject org.onyxplatform/onyx-amazon-sqs "0.11.0.0-alpha5"
   :description "Onyx plugin for Amazon SQS"
   :url "https://github.com/onyx-platform/onyx-amazon-sqs"
   :license {:name "Eclipse Public License"
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.0-alpha4"]
+                 [org.onyxplatform/onyx "0.11.0-alpha5"]
                  [com.amazonaws/aws-java-sdk "1.11.76"]]
   :global-vars  {*warn-on-reflection* true}
   :resource-paths ["resources/"]
