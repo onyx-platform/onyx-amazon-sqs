76b127d25288dea06db00d1384533888e569be29
OnyxBot <michael.drogalis@onyxplatform.org>
2017-09-29 01:26:23 +0000

    Update dependency org.onyxplatform/onyx to version 0.11.1-20170929_011639-g969b23c.
    Automatic commit by lein-unison.

--- project.clj
+++ project.clj
@@ -13,7 +13,7 @@
                              :sign-releases false}}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  ^{:voom {:repo "git@github.com:onyx-platform/onyx.git" :branch "master"}}
-                 [org.onyxplatform/onyx "0.11.1-20170928_061857-ge055991"]
+                 [org.onyxplatform/onyx "0.11.1-20170929_011639-g969b23c"]
                  [com.amazonaws/aws-java-sdk "1.11.76"]]
   :global-vars  {*warn-on-reflection* true}
   :resource-paths ["resources/"]
